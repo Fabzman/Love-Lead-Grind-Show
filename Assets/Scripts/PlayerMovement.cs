e9046fbbc456725048c8cf40ac4acf44e79a6a18
Fabzman <Fabian.Est.Figueroa@gmail.com>
2018-07-20 17:55:42 +1000

    Player Movement Script
    Successfully got player moving, next up looking in the direction of the mouse and shooting

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float Speed;
+    private Rigidbody rigidBody;
+    private Vector3 movement;
+    private Vector3 moveSpeed;
+
+	// At the beginning call the Rigidbody on the Player
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// When it updates detect what direction is being pressed on the keyboard
+	void Update ()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveSpeed = movement * Speed;
+    }
+
+    //Updates for physics (i think)
+    void FixedUpdate()
+    {
+        rigidBody.velocity = moveSpeed;
+    }
+}
